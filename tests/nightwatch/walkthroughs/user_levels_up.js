950de57f26dcd9f27b5de47ba7ccd26936cc7dc0
Heitor Salazar <heitorsalazar@gmail.com>
2015-03-05 12:07:42 -0300

    add level command, update login command, and levels up tests

--- tests/nightwatch/walkthroughs/user_levels_up.js
+++ tests/nightwatch/walkthroughs/user_levels_up.js
@@ -4,25 +4,25 @@ module.exports = {
         return client
             .login()
             .pause(500)
-        
+
             .assert.containsText('body', "NÍVEL\n1")
-        
+
             .getAttribute("paper-progress", 'value', function (result) {
                 experience = result.value
-            })    
+            })
             .click("#fill.skill paper-button")
-            
+
             .assert.containsText('body', "NÍVEL\n2")
-            
-            // TODO test progress bar going to the exact XP? 
+
+            // TODO test progress bar going to the exact XP?
             .getAttribute("paper-progress", 'value', function (result) {
                 this.assert.equal(result.value, 0, "Progress bar is at zero.");
             })
-            
+
             .end();
     },
-    
-    
+
+
     "Level up associated skills": function(client) {
         console.log("Pending")
         return client.end();
--- tests/nightwatch/walkthroughs/user_levels_up_skill.js
+++ tests/nightwatch/walkthroughs/user_levels_up_skill.js
@@ -2,9 +2,9 @@ module.exports = {
     "User sees feedback of leveling up": function(client) {
         return client
             .login()
-            .pause(500)
             // As of now, skill level changes with user level
-            .assert.containsText('body', 'Level: 2')
+            .level(2)
+            .moveToElement('#drink.skill')
             .assert.containsText('#drink.skill', '(POUCO)')
             .click("#drink.skill paper-button")
             .pause(200)
@@ -16,7 +16,7 @@ module.exports = {
             .pause(200)
             .click("#drink.skill paper-button")
             .pause(200)
-            .assert.containsText('body', 'Level: 3')
+            .level(3)
             .assert.containsText('#drink.skill', '(MÉDIO)')
             .end();
     }
